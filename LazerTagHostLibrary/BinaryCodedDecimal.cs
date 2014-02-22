53928458505bb2e609b0fdc59655c755ff794eea
Riley McArdle <riley@glyff.net>
2014-02-22 14:16:02 -0600

    Change BinaryCodedDecimal class to struct and implement IConvertible interface.

--- LazerTagHostLibrary/BinaryCodedDecimal.cs
+++ LazerTagHostLibrary/BinaryCodedDecimal.cs
@@ -1,11 +1,12 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 
 namespace LazerTagHostLibrary
 {
 	[ImmutableObject(true)]
-	public class BinaryCodedDecimal
+	public struct BinaryCodedDecimal : IConvertible
 	{
-		public BinaryCodedDecimal(int value, bool binaryCoded = false)
+		public BinaryCodedDecimal(int value, bool binaryCoded = false) : this()
 		{
 			DecimalValue = binaryCoded ? ToDecimal(value) : (byte) value;
 		}
@@ -39,6 +40,91 @@ namespace LazerTagHostLibrary
 		{
 			if (dec < 0 || dec >= 0xff) return 0xff;
 			return (byte)(((dec / 10) << 4) | (dec % 10));
+		}
+
+		public TypeCode GetTypeCode()
+		{
+			return TypeCode.Byte;
+		}
+
+		public bool ToBoolean(IFormatProvider provider)
+		{
+			return Convert.ToBoolean(DecimalValue, provider);
+		}
+
+		public char ToChar(IFormatProvider provider)
+		{
+			return Convert.ToChar(DecimalValue, provider);
+		}
+
+		public sbyte ToSByte(IFormatProvider provider)
+		{
+			return Convert.ToSByte(DecimalValue, provider);
+		}
+
+		public byte ToByte(IFormatProvider provider)
+		{
+			return Convert.ToByte(DecimalValue, provider);
+		}
+
+		public short ToInt16(IFormatProvider provider)
+		{
+			return Convert.ToInt16(DecimalValue, provider);
+		}
+
+		public ushort ToUInt16(IFormatProvider provider)
+		{
+			return Convert.ToUInt16(DecimalValue, provider);
+		}
+
+		public int ToInt32(IFormatProvider provider)
+		{
+			return Convert.ToInt32(DecimalValue, provider);
+		}
+
+		public uint ToUInt32(IFormatProvider provider)
+		{
+			return Convert.ToUInt32(DecimalValue, provider);
+		}
+
+		public long ToInt64(IFormatProvider provider)
+		{
+			return Convert.ToInt64(DecimalValue, provider);
+		}
+
+		public ulong ToUInt64(IFormatProvider provider)
+		{
+			return Convert.ToUInt64(DecimalValue, provider);
+		}
+
+		public float ToSingle(IFormatProvider provider)
+		{
+			return Convert.ToSingle(DecimalValue, provider);
+		}
+
+		public double ToDouble(IFormatProvider provider)
+		{
+			return Convert.ToDouble(DecimalValue, provider);
+		}
+
+		public decimal ToDecimal(IFormatProvider provider)
+		{
+			return Convert.ToDecimal(DecimalValue, provider);
+		}
+
+		public DateTime ToDateTime(IFormatProvider provider)
+		{
+			return Convert.ToDateTime(DecimalValue, provider);
+		}
+
+		public string ToString(IFormatProvider provider)
+		{
+			return Convert.ToString(DecimalValue, provider);
+		}
+
+		public object ToType(Type conversionType, IFormatProvider provider)
+		{
+			return Convert.ChangeType(DecimalValue, conversionType, provider);
 		}
 	}
 }
